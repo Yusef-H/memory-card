e7bf43e5b9d29dd210a65b6495c3405ed71c861f
Joseph <yusefhalabi456@gmail.com>
2023-07-25 12:48:48 +0300

    fix scores & add shuffle animals

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
-import animals from '../animals'
+import {animals, shuffleArray} from '../animals'
 import '../assets/styles/game.css'
 
 function Game() {
     const [score, setScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
     const [animalsState, setAnimals] = useState(animals);
-  
     useEffect(() => {
         // Load the best score from local storage when the component mounts
         const storedBestScore = localStorage.getItem('bestScore');
         if (storedBestScore) {
-        setBestScore(parseInt(storedBestScore));
+            setBestScore(parseInt(storedBestScore));
+        } else {
+            // If no best score is found in local storage, set it to 0
+            setBestScore(0);
         }
-    }, []);
+    });
   
     const handleAnimalClick = (clickedAnimal) => {
-        console.log(clickedAnimal);
+
         // If the clicked animal was previously clicked (isClicked is true), reset the score
         if (clickedAnimal.isClicked) {
             setScore(0);
@@ -27,16 +29,30 @@ function Game() {
                 // Save the new best score in local storage
                 localStorage.setItem('bestScore', score.toString());
             }
+            restartGame();
+            return;
         }
         setScore((prevScore) => prevScore + 1);
         // Find the clicked animal in the animals array and check if it was previously clicked
         const updatedAnimals = animalsState.map((animal) =>
             animal.id === clickedAnimal.id ? { ...animal, isClicked: true } : animal
         );
+        const shuffledAnimals = shuffleArray(updatedAnimals);
 
         // Update the animals state with the updatedAnimals array
-        setAnimals(updatedAnimals);
+        setAnimals(shuffledAnimals);
     };
+
+    const restartGame = () => {
+        const updatedAnimals = animalsState.map((animal) => ({
+          ...animal,
+          isClicked: false,
+        }));
+      
+        setAnimals(updatedAnimals);
+        setScore(0);
+    };
+      
   
     return (
         <div>
